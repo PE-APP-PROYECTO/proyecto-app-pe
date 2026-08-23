30a312bda4b3002cf05e0e98b8a837bd0e43b208
wilmarito <wilmar.06@gmail.com>
2026-08-23 00:03:40 -0500

    fix: se agrega codigo faltante

--- back-end/app/models/__init__.py
+++ back-end/app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.user import User
 from app.models.brand import Brand
 from app.models.provider import Provider
 from app.models.product import Product
+from app.database import Base
 
-__all__ = ["User", "Brand", "Provider", "Product"]
+__all__ = ["Base", "User", "Brand", "Provider", "Product"]
