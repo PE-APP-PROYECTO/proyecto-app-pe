b6d7dc635f6dd7476a327efee68474caf8c5a840
Wilmar Arley Ramirez Gil <wilmar.0614@gmail.com>
2026-08-23 22:33:34 -0500

    Revert "Feature/controller"

--- back-end/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.brand import Brand
-from app.models.provider import Provider
-from app.models.product import Product
-
-__all__ = ["User", "Brand", "Provider", "Product"]
