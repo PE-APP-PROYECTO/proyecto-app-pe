d0723d8dd1bbdc198544e888542f742f51f94366
wilmarRamirez <wilmar.06@hotmail.com>
2026-08-27 10:32:10 -0500

    feat: se agrega configuracion para imprimir la fecha de la peticion

--- back-end/app/core/exception_handlers.py
+++ back-end/app/core/exception_handlers.py
@@ -1,13 +1,18 @@
-# app/core/exception_handlers.py
+import logging
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
-
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 # Importa tus excepciones personalizadas
 from app.utils.exceptions import (
     NotFoundError,
     UnauthorizedError,
     ValidationError,
 )
+COLOMBIA_TZ = ZoneInfo("America/Bogota")
+logger = logging.getLogger("tecApp")
 
 def register_exception_handlers(app: FastAPI) -> None:
     """Centraliza e inscribe todos los Exception Handlers en la aplicación FastAPI."""
@@ -31,4 +36,20 @@ def register_exception_handlers(app: FastAPI) -> None:
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={"detail": str(exc)},
+        )
+
+   # Dentro de tu función de registro:
+    @app.exception_handler(StarletteHTTPException)
+    async def http_exception_handler(request: Request, exc: StarletteHTTPException):
+        fecha_hora_co = datetime.now(COLOMBIA_TZ).strftime("%Y-%m-%d %H:%M:%S")
+        logger.warning(f"[{fecha_hora_co}] HTTP {exc.status_code} en {request.method} {request.url.path}: {exc.detail}")
+
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={
+                "timestamp": fecha_hora_co,
+                "timezone": "America/Bogota (UTC-5)",
+                "path": request.url.path,
+                "detail": exc.detail
+            }
         )
